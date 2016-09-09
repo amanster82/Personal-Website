7be34435aae5e362128231b86d4b79c7d6d3d8c6
Aman Bhayani <amanbhayani@hotmail.com>
2016-09-08 18:23:44 -0700

    Working on modual for skills

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -154,6 +154,8 @@ var $arrow= $('#arrow').addClass('doThis');
 
 
 
+//about
+
 var $aboutsection= $('#about img');
 
 $aboutsection.waypoint(function(){
@@ -162,7 +164,7 @@ $aboutsection.waypoint(function(){
 
 }, { offset: '70%' });
 
-
+//Service
 var $communication= $('#services i.fa.communication');
 
 $communication.waypoint(
@@ -183,12 +185,24 @@ $communication.waypoint(
 
 var $innovation= $('#services i.fa.innovation');
 
-$communication.waypoint(
+$innovation.waypoint(
     function(){
         $innovation.stop().delay(2000).animate({color: 'white', backgroundColor: '#FCAC45'}, 1000).animate({color: '#333', backgroundColor: '#fff'}, 1000);
         this.destroy();
 }, { offset: '70%' });
 
+
+//Projects
+
+
+var $projects= $('#project .img-parent img');
+
+$projects.waypoint(
+    function(){
+    setTimeout(function(){
+       $projects.stop().removeClass('imgblur'); 
+    }, 1000); 
+}, { offset: '70%' });
 
 
 var $map= $('.map-thing');
@@ -207,3 +221,62 @@ $contact.waypoint(function(){
     console.log("HELLLOOOOOOOOOO");
 }, { offset: '90%' });
 
+
+
+//-----------------------------------------------
+
+// Progress bars
+
+
+// progressbar.js@1.0.0 version is used
+// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+
+
+
+require.config({
+    paths: {'progressbar': '../bower_components/progressbar.js/dist/progressbar'}
+});
+
+define(['progressbar'], function(ProgressBar) {
+    var line = new ProgressBar.Line('#something');
+});
+
+
+
+$(function(){
+
+var ProgressBar = require ('progressbar.js'); 
+
+var bar = new ProgressBar.Circle('#something', {
+  color: '#aaa',
+  // This has to be the same size as the maximum width to
+  // prevent clipping
+  strokeWidth: 4,
+  trailWidth: 1,
+  easing: 'easeInOut',
+  duration: 1400,
+  text: {
+    autoStylesomething: false
+  },
+  from: { color: '#aaa', width: 1 },
+  to: { color: '#333', width: 4 },
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+    circle.path.setAttribute('stroke-width', state.width);
+
+    var value = Math.round(circle.value() * 100);
+    if (value === 0) {
+      circle.setText('');
+    } else {
+      circle.setText(value);
+    }
+
+  }
+});
+bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+bar.text.style.fontSize = '2rem';
+
+bar.animate(1.0);  // Number from 0.0 to 1.0
+
+});
